e6249ab143bf67661ea564ccfed7b1751ded55cb
apierson3 <apierson3@gmail.com>
2020-10-17 16:26:22 -0500

    Change text and comment

--- Website.py
+++ Website.py
@@ -5,12 +5,12 @@ from flask import Flask
 # Create the new app for the running process and various tasks required for the website
 app = Flask(__name__)
 
-# Add the decorator
+# Add the decorator for the webpage hierarchy
 @app.route('/')
 
 # Define a function with no parameters that returns the text in parenthesis
 def home():
-    return "Chatbot Website"
+    return "Chatbot Website - Andrew Pierson"
 
 # This statement runs the file only if the whole code is executed
 if __name__ =='__main__':
