1e8fb95032a818fa88e26f9e7343d040822c4fac
apierson3 <apierson3@gmail.com>
2020-10-17 08:31:59 -0500

    Create Flask app with text

--- Website.py
+++ Website.py
@@ -1,2 +1,17 @@
 # This is the website front-end for Azure my chatbot
-print('hello jennifer')
+# Import libraries and dependencies
+from flask import Flask
+
+# Create the new app for the running process and various tasks required for the website
+app = Flask(__name__)
+
+# Add the decorator
+@app.route('/')
+
+# Define a function with no parameters that returns the text in parenthesis
+def home():
+    return "Chatbot Website"
+
+# This statement runs the file only if the whole code is executed
+if __name__ =='_main_':
+    app.run(host='0.0.0.0', debug=True)
